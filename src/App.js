851411833275c3d6570ccdc5febdc6b74bf9fb0f
Bruno de Oliveira <bruno.mktd@gmail.com>
2019-09-22 14:18:15 -0300

    Aula ReactJS seguindo tutorial Tania Rascia

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Table from './Table';
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+//Com JSX
+// const heading = <h1 className="site-heading">Hello React JSX</h1>
+
+// //SEM JSX
+// const name = "Bruno";
+// const heading2 = React.createElement('h1', {className: 'site-heading'}, `Hello ${name}, React Sem JSX`)
+
+class App extends Component {
+
+  state = {
+    characters: [],
+  }
+
+  removeCharacter = (index) =>{
+    const {characters} = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i)=>{
+        return i !== index
+      })
+    })
+  }
+
+  handleSubmit = (character) =>{
+    this.setState({characters: [...this.state.characters, character]})
+  }
+
+    render() {
+      // const characters = [
+      //   {
+      //     name: 'Charlie',
+      //     job: 'Arquiteto',
+      //   },
+      //   {
+      //     name: 'Mac',
+      //     job: 'Vendedor',
+      //   },
+      //   {
+      //     name: 'Dee',
+      //     job: 'Ator',
+      //   },
+      //   {
+      //     name: 'Dennis',
+      //     job: 'Bartender',
+      //   },
+      // ]
+
+      const {characters} = this.state
+      
+      return (
+        <div className="container">
+          <Table characterData = {characters} removeCharacter={this.removeCharacter}/>
+          <Form handleSubmit={this.handleSubmit}/>
+        </div>
+      )
+    }
+  }
 
 export default App;
